a97af289cd4c2dbb6b41ec95a8790fb28e1993a8
sowens23 <thespencerowens@outlook.com>
2024-04-07 19:24:22 -0800

    [hw] assingment5 finished: haskell

--- homework/assignment5/PA5.hs
+++ homework/assignment5/PA5.hs
@@ -1,13 +1,16 @@
--- Secret message #4:
--- Every earless elephant eagerly eats endive early each evening.
-
 -- PA5.hs
 -- Spencer Baysinger
 -- 2024-04-05
---
+
 -- For CS 331 Spring 2024
+
+-- Assignment 5, part A - Haskell Variables & Functions
+-- Secret message #4:
+-- Every earless elephant eagerly eats endive early each evening.
+
 -- Assignment 5, part B - Haskell Variables & Functions
--- This assignment we will demonstrate Haskell knowledge by creating a module containnig functionality to deal with lists and numbers.
+-- This assignment we will demonstrate Haskell knowledge by creating a module 
+-- containing functionality to deal with lists and numbers.
 
 -- File based on PA5.hs (skeleton) written by Professor Glenn Chappell
         -- PA5.hs  SKELETON
@@ -19,37 +22,41 @@
 
 module PA5 where
 
-
+import Data.List (isPrefixOf, tails, findIndex)
 -- =====================================================================
-
 
 -- collatzCounts
+
 collatzCounts :: [Integer]
-collatzCounts = [42..]  -- DUMMY; REWRITE THIS!!!
+collatzCounts = map collatz [1..]
 
-
--- =====================================================================
-
-
--- operator <#
-(<#) :: Ord a => [a] -> [a] -> Int
-_ <# _ = 42  -- DUMMY; REWRITE THIS!!!
-
+collatz :: Integer -> Integer
+collatz 1 = 0
+collatz n
+    | even n    = 1 + collatz (n `div` 2)
+    | otherwise = 1 + collatz (3*n + 1)
 
 -- =====================================================================
-
 
 -- filter2
 filter2 :: (a -> Bool) -> [a] -> [b] -> [b]
-filter2 _ _ bs = bs  -- DUMMY; REWRITE THIS!!!
-
+filter2 p xs ys = [y | (x, y) <- zip xs ys, p x]
 
 -- =====================================================================
 
+-- operator <#
+(<#) :: Ord a => [a] -> [a] -> Int
+xs <# ys = length [1 | (x, y) <- zip xs ys, x < y]
+
+-- =====================================================================
 
 -- listSearch
 listSearch :: Eq a => [a] -> [a] -> Maybe Int
-listSearch _ _ = Just 42  -- DUMMY; REWRITE THIS!!!
+listSearch [] _ = Just 0
+listSearch _ [] = Nothing
+listSearch xs ys
+    | length xs > length ys = Nothing
+    | otherwise = findIndex (isPrefixOf xs) (tails ys)
 
 
 -- =====================================================================
@@ -67,5 +74,10 @@ concatEvenOdd :: [String] -> (String, String)
   Above, "..." should be replaced by other code. "fold*" must be one of
   the following: foldl, foldr, foldl1, foldr1.
 -}
-concatEvenOdd _ = ("Yo", "Yoyo")  -- DUMMY; REWRITE THIS!!!
+concatEvenOdd xs = foldl f ("", "") (zip [0..] xs)
+  where
+    f (evens, odds) (index, value)
+      | even index = (evens ++ value, odds)
+      | otherwise  = (evens, odds ++ value)
+
 
--- homework/assignment5/computemax.hs
+++ homework/assignment5/computemax.hs
@@ -1,12 +1,48 @@
--- Secret message #4:
--- Every earless elephant eagerly eats endive early each evening.
-
--- parseit.lua  
+-- computemax.lua  
 -- Spencer Baysinger
--- 2024-02-20
+-- 2024-04-07
 --
 -- For CS 331 Spring 2024
--- Solution to Assignment 4, Exercise A
--- Requires lexit.lua
+-- Solution to Assignment 5, Exercise C
+-- File is written to demonstrate Haskell understanding
+-- Print an explanatory message.
+    -- Input a sequence of integers from the user, one on each line, ending with a blank line.
+    -- If the list is empty, then print a message indicating this. Otherwise, print the maximum item in the list.
+    -- Ask the user whether to repeat the program. If so, start over; if not, exit.
 
--- File based on parseit.lua (skeleton) written by Professor Glenn Chappell
+import Control.Monad (unless)
+import Text.Read (readMaybe)
+
+main :: IO ()
+main = do
+  putStrLn ""
+  putStrLn "-------------------------------------------------------------------------"
+  putStrLn "-- This program will input an arbitrary amount of integers into a list --"
+  putStrLn "--      And then it will output the maximum integer in that list.      --"
+  putStrLn "-------------------------------------------------------------------------"
+  putStrLn ""
+  nums <- inputNums
+  putStrLn ""
+  case nums of
+    [] -> putStrLn "(Empty list) No Maximum"
+    _  -> putStrLn $ "Maximum: " ++ show (maximum nums)
+  putStrLn ""
+  askRepeat
+
+askRepeat :: IO ()
+askRepeat = do
+  putStr "Compute another maximum? (y/n): "
+  decision <- getLine
+  unless (decision /= "y") $ main
+
+inputNums :: IO [Int]
+inputNums = go []
+  where
+    go acc = do
+      putStr "Enter an integer: "
+      line <- getLine
+      case line of
+        "" -> return $ reverse acc
+        _ -> case readMaybe line of
+          Just num -> go (num : acc)
+          Nothing -> putStrLn "Invalid input, please enter an integer." >> go acc
